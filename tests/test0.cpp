759753fa6ce236aa4db011a2ab4787a0e45aea38
songxu <2237505658@qq.com>
2022-05-24 23:34:50 +0800

    修改测试

--- tests/test0.cpp
+++ tests/test0.cpp
@@ -1,20 +1,53 @@
 #include <iostream>
+#include <cassert>
 #include "../include/json.hpp"
 using json = sjson::json;
+
+#define ASSERT(arg) assert(arg)
 
 int main()
 {
     json obj = {
-        {
-            "user", {
-                { "id", 10 },
-                { "name", "sjson" },
-                { "version", "0.1.0" }
+        {"test", 233},
+        {"hello", "world"},
+        {"obj", {
+            {"000", 123}
             }
         }
     };
 
-    auto j1 = json::object({"test", 233});
+    std::cout << obj << "\n";
+
+
+    using ObjectType = std::map<std::string, json>;
+    using KEY_TYPE = decltype("test");
+    constexpr auto is_cont = std::is_constructible<typename ObjectType::key_type, KEY_TYPE>::value;
+    auto v_test = obj["test"];
+    ASSERT(v_test.get<int>() == 233);
+
+
+    auto v_obj = obj["obj"];
+    auto v_000 = v_obj["000"];
+    ASSERT(v_000.get<int>() == 123);
+
+    json j0 = nullptr;
+    json j1 = {"pi", 3.14};
+    json j2 = "json test";
+    json j3 = 233;
+    json j4 = 1.414;
+    json j5 = true;
+
+
+    double v40 = j4;
+    auto v4 = static_cast<double>(j4);
+    ASSERT(v4 == 1.414);
+
+    auto v3 = j3.get<int>();
+    ASSERT(v3 == 233);
+
+
+    auto j10 = json::object({"test", 233});
+    auto j11 = json::array({0, 1, 2, 3});
 
 
     return 0;
